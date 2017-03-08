243e9901f166f1e5c531661c79f36526d628ba28
gaspar <gualberto12gaspar@gmail.com>
2017-03-08 11:02:44 -0400

    resolviendo conflictos routes

--- app/models/student_card.rb
+++ app/models/student_card.rb
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
+
 class StudentCard < ApplicationRecord
-=======
-class StudentCard < ApplicationRecord::Base
-  validates :user_name, presence: true,length: { maximum: 50}
-  validates :password, presence: true, length: { maximum: 50}
-   belongs_to :student
->>>>>>> f836c0bf449832fa4e0f3b42b769333e770e6a66
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :student_cards
-=======
-  get 'login/index'
-
->>>>>>> f836c0bf449832fa4e0f3b42b769333e770e6a66
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
