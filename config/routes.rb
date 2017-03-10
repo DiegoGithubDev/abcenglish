1bd71193f6f67bb2f7f647727d3f55ba7e593b5f
Rigoberto <rig.tapia@hotmail.com>
2017-03-10 14:49:00 -0400

    Only one image is seen in the part of the slide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :student_cards
+  root 'student_cards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
