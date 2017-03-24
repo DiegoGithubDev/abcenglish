d78fb7d75868c6a2d252a2b5ef3937887fb17c2a
Bejarano <mauriciobejarano@localhost.localdomain>
2017-03-24 10:45:13 -0400

    write business( login )  in controler StudentSessions

--- app/controllers/student_sessions_controller.rb
+++ app/controllers/student_sessions_controller.rb
@@ -14,11 +14,14 @@ class StudentSessionsController < ApplicationController
     #render :text => params[:password].inspect
     #@studentx= Student.find(params[:user_name])
     #@studentx= Student.find_by(user_name: params[:user_name],password: params[:password]) error por la encripatacion
-    @studentx= Student.find_by(user_name: params[:user_name])
+    @studentx= Student.find_by(user_name: params[:user_name],password: params[:password])
     if @studentx != nil
-     render :text => params[:user_name].inspect + @studentx.to_s
+     render :text => 'vista dashboard'
     else
-      render :text =>' no existe el ususario'
+      @student_cards = StudentCard.find_by(user_name: params[:user_name],password: params[:password])
+      if @student_cards!=nil
+        render :text => 'registrese por favor'
+      end
     end
       #else
      # render :text => params[:user_name].inspect + "no existe"
