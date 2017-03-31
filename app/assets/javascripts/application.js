d65f7497878315cfae85ee9693d0ad7271bc639c
Bejarano <mauriciobejarano@localhost.localdomain>
2017-03-31 14:53:59 -0400

    being to do sideNav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,17 @@
 //= require bxslider
 //= require materialize-sprockets
 //= require_tree .
+$(document).ready(function(){
+// Next slide
+    $('.carousel').carousel();
+    $(".button-collapse").sideNav();
+
+    /*$('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        }
+    );*/
+
+});
