d1be744376cde94f39d3281eaa4e92e8b92f2193
Bejarano <mauriciobejarano@localhost.localdomain>
2017-04-21 12:59:38 -0400

    delete code in registrations controller

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -1,8 +1,6 @@
 class Students::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
    before_action :configure_account_update_params, only: [:update]
-   before_action :go_to_login, only: [:new]
-
 
    # GET /resource/sign_up
    def new
