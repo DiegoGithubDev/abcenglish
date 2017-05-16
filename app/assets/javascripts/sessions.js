867495e32c255bb6c5e5086552f771269ba9ff69
Bejarano <mauriciobejarano@localhost.localdomain>
2017-05-16 15:51:15 -0400

    margin-right 20px slider imagen, and modifi formule for centrar login

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -14,7 +14,7 @@
         var documentoAltura=$(document).height();
         var filaHaltura= document.getElementById("rowCenter").offsetHeight;
         var sobrante=documentoAltura-filaHaltura;
-        var puntox=(sobrante/2);
+        var puntox=(sobrante/2-10);
         document.getElementById("rowCenter").style.marginTop=puntox+"px";
 
 
