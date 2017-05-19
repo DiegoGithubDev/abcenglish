2bd465c265264f0cf905ec03836a94a0ec0455c3
Bejarano <mauriciobejarano@localhost.localdomain>
2017-05-19 15:45:33 -0400

    student controller registration: add fields permit facebook and skype

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -46,13 +46,13 @@ class Students::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
      #devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image])
-     params.require(:student).permit(:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image,:address, :phone)
+     params.require(:student).permit(:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image,:address, :phone, :facebook, :skype)
    end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
      #devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image])
-     params.require(:student).permit(:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image, :address, :phone)
+     params.require(:student).permit(:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image, :address, :phone, :facebook, :skype)
    end
 
 
