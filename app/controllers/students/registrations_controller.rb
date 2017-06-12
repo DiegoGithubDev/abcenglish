6ae4d6d3896111f6ae2940b6bb1e981a5a4b5f94
Diego Mansilla <mauriciobejarano@localhost.localdomain>
2017-06-12 11:17:02 -0400

    update fields ok

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -52,7 +52,7 @@ class Students::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     #devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image, :address, :phone, :facebook, :skype])
      params.require(:student).permit(:name, :last_name,:user_name, :email, :password, :password_confirmation, :current_password, :image, :address, :phone, :facebook, :skype)
    end
 
