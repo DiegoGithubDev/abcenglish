8771adb2d88f53d79498595a284f01938c147dd3
Diego Mansilla <mauriciobejarano@localhost.localdomain>
2017-06-13 10:51:41 -0400

    modifiq filter in controler dashboard

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Students::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
    before_action :configure_account_update_params, only: [:update]
-   before_action :go_to_login
+   before_action :go_to_login, only: [:new,:create]
 
    # GET /resource/sign_up
    def new
