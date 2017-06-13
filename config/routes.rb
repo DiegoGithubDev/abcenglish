fd5e0e52297b01feea3d79193d625bf8658e1d72
Diego Mansilla <mauriciobejarano@localhost.localdomain>
2017-06-13 16:18:18 -0400

    add route resources and add commente relod_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
    #get '/login',  to: 'student_sessions#new'
   #get '/logout', to: 'student_sessions#destroy'
-
+  root to: 'dashboard#index'
   get "dashboard/index"
   # registrations: 'registrations',
   devise_for :students, :controllers => {sessions: 'students/sessions',
                                          registrations: 'students/registrations'
 
   }
+  resources :students
 
   #root :to => redirect("/dashboard/index")
-  root 'dashboard#index'
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 		 
