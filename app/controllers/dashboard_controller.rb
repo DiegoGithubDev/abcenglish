475e5dfcacaf5f9700cec783bdb2ae8acb84daa3
Diego Mansilla <mauriciobejarano@localhost.localdomain>
2017-06-30 10:18:15 -0400

    get current page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_student!
   def index
+    @page_current= current_student.current_page
   end
 
   def save
@@ -8,4 +9,21 @@ class DashboardController < ApplicationController
 
   def book_container
   end
+
+
+  def right
+    @page_current = 2 #get current page from student
+    @page_current =  @page_curent + 1
+
+    render "dashboard/page2"
+  end
+
+  def left
+    @page_current = 2 #get current page from student
+    @page_current =  @page_current - 1
+
+    render "dashboard/page1"
+  end
+
+
 end
